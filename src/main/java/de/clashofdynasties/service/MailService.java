579aa64ced898f8a679fcd9aa181707ff6c995b7
jorros <joergrosenauer@gmail.com>
2014-10-18 21:32:23 +0200

    Exception für MailService

--- src/main/java/de/clashofdynasties/service/MailService.java
+++ src/main/java/de/clashofdynasties/service/MailService.java
@@ -16,27 +16,32 @@ public class MailService {
     private JavaMailSenderImpl mailSender;
 
     public void sendMail(Player player, String subject, String content) {
-        mailSender.send(new MimeMessagePreparator() {
-            public void prepare(MimeMessage mimeMessage) throws MessagingException {
-                if(player.getEmail() != null && !player.getEmail().isEmpty()) {
-                    MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8");
-                    message.setFrom("info@clashofdynasties.de");
-                    message.setTo(player.getEmail());
-                    message.setSubject("Clash of Dynasties: " + subject);
+        try {
+            mailSender.send(new MimeMessagePreparator() {
+                public void prepare(MimeMessage mimeMessage) throws MessagingException {
+                    if (player.getEmail() != null && !player.getEmail().isEmpty()) {
+                        MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                        message.setFrom("info@clashofdynasties.de");
+                        message.setTo(player.getEmail());
+                        message.setSubject("Clash of Dynasties: " + subject);
 
-                    String text = "<html>";
-                    text += "<head>";
-                    text += "</head>";
-                    text += "<body>";
-                    text += "<p>Hallo " + player.getName() + ".</p>";
-                    text += "<p>" + content + "</p>";
-                    text += "<p style=\"font-size:11px; margin-top:15px;\">Wenn du in Zukunft diese Mail nicht mehr bekommen möchtest, log dich in das Spiel ein und entferne das jeweilige H&auml;ckchen unter Profil!</p>";
-                    text += "</body>";
-                    text += "</html>";
+                        String text = "<html>";
+                        text += "<head>";
+                        text += "</head>";
+                        text += "<body>";
+                        text += "<p>Hallo " + player.getName() + ".</p>";
+                        text += "<p>" + content + "</p>";
+                        text += "<p style=\"font-size:11px; margin-top:15px;\">Wenn du in Zukunft diese Mail nicht mehr bekommen möchtest, log dich in das Spiel ein und entferne das jeweilige H&auml;ckchen unter Profil!</p>";
+                        text += "</body>";
+                        text += "</html>";
 
-                    message.setText(text, true);
+                        message.setText(text, true);
+                    }
                 }
-            }
-        });
+            });
+        }
+        catch(Exception ignored) {
+
+        }
     }
 }
