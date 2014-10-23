9eb37ddf0453a763fcb92f55bbb51509ec8bf792
jorros <joergrosenauer@gmail.com>
2014-10-23 13:21:23 +0200

    Sight wird korrekt geupdatet

--- src/main/java/de/clashofdynasties/logic/DiplomacyLogic.java
+++ src/main/java/de/clashofdynasties/logic/DiplomacyLogic.java
@@ -28,19 +28,19 @@ public class DiplomacyLogic {
                 Player pl1 = relation.getPlayer1();
                 Player pl2 = relation.getPlayer2();
 
-                if(relation.getRelation() == 3) {
+                relation.setRelation(relation.getRelation() - 1);
+
+                if(relation.getRelation() == 2) {
                     eventRepository.add(new Event("DiplomaticAlliance", "Allianz aufgelöst", "Die Allianz mit " + pl2.getName() + " wurde endgültig aufgelöst! Ihr seid ab sofort nur noch Handelspartner.", "diplomacy?pid=" + pl2.getId(), pl1));
                     eventRepository.add(new Event("DiplomaticAlliance", "Allianz aufgelöst", "Die Allianz mit " + pl1.getName() + " wurde endgültig aufgelöst! Ihr seid ab sofort nur noch Handelspartner.", "diplomacy?pid=" + pl1.getId(), pl2));
                 }
-                else if(relation.getRelation() == 2) {
+                else if(relation.getRelation() == 1) {
                     eventRepository.add(new Event("DiplomaticTrade", "Handelsvertrag aufgelöst", "Das Handelsabkommen mit " + pl2.getName() + " ist nicht mehr gültig!.", "diplomacy?pid=" + pl2.getId(), pl1));
                     eventRepository.add(new Event("DiplomaticTrade", "Handelsvertrag aufgelöst", "Das Handelsabkommen mit " + pl1.getName() + " ist nicht mehr gültig!.", "diplomacy?pid=" + pl1.getId(), pl2));
-
-                    pl1.setSightUpdate(true);
-                    pl2.setSightUpdate(true);
                 }
 
-                relation.setRelation(relation.getRelation() - 1);
+                pl1.setSightUpdate(true);
+                pl2.setSightUpdate(true);
             }
         }
     }
